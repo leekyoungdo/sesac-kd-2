3ec595c056f67a7ccd5b25b124784ad4e0955c2b
“leekyoungdo” <flrudeh@naver.com>
2023-10-10 18:13:59 +0900

    study for node js module

---
+++ HtmlCssJs/node-test/test.js
@@ -0,0 +1,23 @@
+// // function first(){
+// //     second();
+// //     console.log(1);
+// //     return;
+// // }
+
+// // function second(){
+// //     console.log(2);
+// //     return;
+// // }
+
+// // first();
+
+
+function run() {
+    console.log("3초뒤 실행");
+}
+
+    console.log("시작")
+
+    //콜백 함수 : 함수의 인자로 함수를 넘길 때 , 인자로 넘기는 그 함수
+    setTimeout(run, 3000)
+    console.log("끝")
