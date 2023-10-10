2c68698deb3a19be9ce723d042c45012a1e03a59
“leekyoungdo” <flrudeh@naver.com>
2023-10-10 18:15:24 +0900

    study for node js module new

--- HtmlCssJs/node-test/index.js
+++ HtmlCssJs/node-test/index.js
@@ -1,30 +1,24 @@
-//--------------case 3)-----------------
-//파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
-//특정의 식별자만 필요한 경우(add,PI) 모듈을 받아올 때
-//
-const { add,PI } = require("./math.js");
+// ----------------- case 3) ---------------
+// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
+// 특정 식별자(add, minus) 만 필요한 경우 객체구조분해 할당 문법을 통해 받아올 수 있다.
+const { add, minus } = require("./math.js");
 
-const sum = add(1, 3);
+const sum = add(2, 3);
 console.log(sum);
-console.log(PI) // 오류
+// console.log(PI); // 오류 발생
 
-//--------------case 2)------------------
-//파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
-//모듈에서 받아올 때 객체를 그대로 math라는 식별자로 받음
-// const math = require("./math.js")
+// ----------------- case 2) ---------------
+// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
+// 모듈을 받아올 때 객체를 그대로 math라는 식별자로 받음.
+// const math = require("./math.js");
 
 // const sum = math.add(3, 5);
-// const minus = math.minus(4, 8)
-// console.log(sum)
-// console.log(math.PI)
-// console.log(minus)
+// console.log(sum);
+// console.log(math.PI);
 
-
-
-
-//--------------case 1)-----------------
+// ----------------- case 1) ---------------
 // 파일에서 한개의 식별자만 내보내는 경우
-// const add =  require("./math.js")
-// 모듈을 불러오는 함수  그 값을  add 에 담는다
+// const add = require("./math.js");
+
 // const sum = add(2, 3);
 // console.log(sum);
