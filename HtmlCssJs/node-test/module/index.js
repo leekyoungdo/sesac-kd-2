ad9b436f0e9a5b8e9b155ad7c908139d7d4ee122
“leekyoungdo” <flrudeh@naver.com>
2023-10-12 18:25:24 +0900

    study for js-hard and module

--- HtmlCssJs/node-test/module/index.js
+++ HtmlCssJs/node-test/module/index.js
@@ -1,10 +1,10 @@
 // ----------------- case 3) ---------------
 // 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
 // 특정 식별자(add, minus) 만 필요한 경우 객체구조분해 할당 문법을 통해 받아올 수 있다.
-const { add, minus } = require("./math.js");
+// const { add, minus, PI } = require("./math.js");
 
-const sum = add(2, 3);
-console.log(sum);
+// const sum = add(2, 3);
+// console.log(sum);
 // console.log(PI); // 오류 발생
 
 // ----------------- case 2) ---------------
@@ -18,7 +18,8 @@ console.log(sum);
 
 // ----------------- case 1) ---------------
 // 파일에서 한개의 식별자만 내보내는 경우
-// const add = require("./math.js");
+const add = require("./math.js");
 
-// const sum = add(2, 3);
-// console.log(sum);
+const sum = add(2, 3);
+console.log(add2);
+console.log(sum)
