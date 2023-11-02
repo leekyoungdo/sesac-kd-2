a919ca1e827b184e9b193d96cbec9b62fe4d45a1
leekyoungdo <flrudeh@naver.com>
2023-11-02 18:33:44 +0900

    study for squelize-2

---
+++ node-backend/06-mvc-mysql solution/model/index.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+const config = require("../config/config.json")["development"];
+// config ={
+//     "host": "localhost",
+//     "database" :"sesac_test",
+//     "username" :"visitor",
+//     "password" :"sba123$",
+//     "dialect" : "mysql"
+// }
+
+const db = {};
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+//sequelize 객체를 만든다.
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+// db = {sequelize: sequelize, Sequelize:Sequelize}
+
+// const Visitor = require("./Visitor")
+// db.Visitor = Visitor(sequelize, Seiquelize)
+
+db.User = require("./User")(sequelize, Sequelize);
+
+//db.Visiotr 에는 sequelize로 Visitor 테이블을 정의한 객체를 담음
+
+// db.User =  require("./Visitor")(sequelize,Sequelize)
+
+module.exports = db;
