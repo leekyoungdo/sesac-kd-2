ae743ce9aefc0c61622f9793a58af135b1b84061
“leekyoungdo” <flrudeh@naver.com>
2023-11-02 18:33:44 +0900

    study for squelize-2

--- node-backend/06-mvc-mysql solution/controller/Cuser.js
+++ node-backend/06-mvc-mysql solution/controller/Cuser.js
@@ -1,4 +1,5 @@
-const User = require('../model/User')
+const {User} = require ("../model")
+
 
 exports.index = (req, res) => {
   res.render('index')
@@ -8,42 +9,66 @@ exports.signup = (req, res) => {
   res.render('signup')
 }
 exports.post_signup = (req, res) => {
-  User.post_signup(req.body, function () {
-    res.send({ result: true })
-  })
+    const data = {
+        userid: req.body.userid,
+        pw : req.body.pw,
+        name : req.body.name
+    }
+    const createUser =  User.create(data)
+    res.send(createUser)
 }
 
 exports.signin = (req, res) => {
   res.render('signin')
 }
 exports.post_signin = (req, res) => {
-  User.post_signin(req.body, function (rows) {
-    console.log(rows[0])
-    if (rows.length > 0) res.send({ result: true, id: rows[0].id })
+    User.findAll({
+      where:{
+        userid: req.body.userid,
+        pw: req.body.pw}
+    })
+    .then((result)=>{
+    if (result.length > 0) res.send({ result: true, id: result[0].id })
     else res.send({ result: false })
-  })
-}
+})
+
+  }
+
 
 exports.profile = (req, res) => {
-  User.get_user(req.body.id, function (result) {
-    console.log('profile', result)
-    if (result.length > 0) res.render('profile', { data: result[0] })
+  User.findOne({
+    where:{
+        id: req.body.id,
+    }
+  }).then((result)=>{
+    if (result) res.render('profile', { data: result })
     else res.redirect('/user/signin')
   })
-}
+    
+  }
+
 
 exports.profile_edit = (req, res) => {
-  const data = {
-    ...req.body,
-    id: req.params.id,
-  }
-  User.update_profile(data, function () {
-    res.send({ result: true })
-  })
+    const data = {
+        ...req.body,
+        id: req.params.id,
+    };
+    User.update(data,{
+        where :{
+            id: data.id,
+        },
+    }).then((result)=> {
+        res.send({result: true})
+    })
 }
 
 exports.profile_delete = (req, res) => {
-  User.delete_user(req.params.id, function () {
-    res.send({ result: true })
-  })
+    User.destroy ({
+        where: {
+            id:req.params.id,
+        },
+    }).then((result)=> {
+        console.log("destroy: " ,result)
+        res.send({result:true})
+    })
 }
--- node-backend/06-mvc-mysql solution/model/User.js
+++ node-backend/06-mvc-mysql solution/model/User.js
@@ -1,44 +1,43 @@
-const mysql = require('mysql')
-const cnn = mysql.createConnection({
-  host: 'localhost',
-  user: 'user',
-  password: 'qwer1234*',
-  database: 'sesac_test',
-})
+// 테이블의 구조를 정의
+// orm은 객체와 db의 table을 매핑
+// sequelize를 이용해서 table의 구조를 정의할 필요가 있음.
 
-exports.post_signup = (data, cb) => {
-  let sql = `INSERT INTO user(userid, name, pw) VALUES('${data.userid}','${data.name}','${data.pw}');`
-  cnn.query(sql, function (err) {
-    if (err) throw err
-    cb()
-  })
+function User(Sequelize, DataTypes) {
+    return Sequelize.define(// 모델(테이블)정의, sequelize 객체의 define
+        "membership", //테이블 이름
+        {
+            id :{  //int not null primary key auto_increment
+                    type:DataTypes.INTEGER,
+                    allowNull: false, // true가 기본값 not null일 경우 false
+                    primaryKey: true, // false가 기본값
+                    autoIncrement: true,
+            },
+            userid: {
+                type:DataTypes.STRING(20),
+                allowNull: true, // true가 기본값으로  안적어도됨 
+            },
+            name: {
+                    type:DataTypes.STRING(10),
+                    allowNull: true, // true가 기본값으로  안적어도됨 
+
+            },
+            pw: {
+                    type : DataTypes.STRING(20),
+                    allowNull: true, // true가 기본값으로  안적어도됨 
+            }   
+        }, //컬럼 정의
+        {
+            tableName : "membership",
+            frezzeTableName: true,
+            // sequelize에서 간혹 단수로 지정해둔 테이블 이름을 sql문을 날릴때 
+            // 복수로 변경을 시켜서 = visitors(처럼) 방지하기 위해 테이블 네임을 고정시키는 
+            // insert into visitor ~~ => create() => insert into visitors ~~~
+            timestamps : false,
+            // insert, update 시에 그 시간을 자동으로 저장하겠다. (저장을 하는것이 기본값)
+            // 저장시키지 않기위해 false로 지정 
+            // createAt, updateAt
+        }
+    )
 }
 
-exports.post_signin = (data, cb) => {
-  let sql = `SELECT * FROM user WHERE userid='${data.userid}' and pw='${data.pw}' limit 1;`
-  cnn.query(sql, function (err, rows) {
-    if (err) throw err
-    cb(rows)
-  })
-}
-exports.get_user = (id, cb) => {
-  let sql = `SELECT * FROM user WHERE id='${id}' limit 1;`
-  cnn.query(sql, function (err, rows) {
-    if (err) throw err
-    cb(rows)
-  })
-}
-
-exports.update_profile = (data, cb) => {
-  let sql = `UPDATE user SET name='${data.name}', pw='${data.pw}' WHERE id='${data.id}'`
-  cnn.query(sql, (err) => {
-    if (err) throw err
-    cb()
-  })
-}
-exports.delete_user = (id, cb) => {
-  cnn.query(`DELETE FROM user WHERE id='${id}'`, (err) => {
-    if (err) throw err
-    cb()
-  })
-}
+module.exports = User;
