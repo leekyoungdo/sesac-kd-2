e88672f024031843ebc39608335d5fb80169ee0d
Kyoung Do Lee <kyoungdolee@Kyoungui-MacBookPro.local>
2023-12-05 18:40:51 +0900

    style Sass study

---
+++ React-proj/05-style/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import OriginCss from './component/OriginCss';
+// import CssModule from './component/CssModule';
+import SassComponent from './component/SassComponent';
+
+function App() {
+  return (
+    <div>
+      {/* <OriginCss /> */}
+      <hr />
+      {/* <CssModule /> */}
+      <hr />
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-proj/05-style/src/component/SassComponent.js
@@ -0,0 +1,33 @@
+import "../styles/origin.scss"
+
+function SassComponent () {
+    return(
+        <>
+        <div className="origin-sass">
+            <div className="box red"></div>
+            <div className="box orange"></div>
+            <div className="box yellow"></div>
+            <div className="box green"></div>
+            <div className="box blue"></div>
+            <div className="box purple"></div>
+        </div>
+        <div className="scss-parents">
+            안녕하세요?
+            <ul>
+                <li>hello</li>
+                <li>world</li>
+                <li>bye</li>
+                <li>world</li>
+            </ul>
+        </div>
+        <div className="box1"></div>
+        <div className="box2"></div>
+
+        <button className="btn">일반버튼</button>
+        <button className="btn-primary">강조버튼</button>
+
+        </>
+    )
+}
+
+export default SassComponent;
