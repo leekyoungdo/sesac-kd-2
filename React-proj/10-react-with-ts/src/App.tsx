8ea75574dff51ded471d04d175c19238fc0ea024
Kyoung Do Lee <kyoungdolee@Kyoungui-MacBookPro.local>
2023-12-14 18:26:28 +0900

    study typescript

---
+++ React-proj/10-react-with-ts/src/App.tsx
@@ -0,0 +1,16 @@
+// import Origin from "./component/Origin";
+// import TsComponent from "./component/TsComponent";
+
+import PostList from "./component/PostList";
+
+function App() {
+  return (
+    <div>
+      {/* <Origin /> */}
+      {/* <TsComponent /> */}
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-proj/10-react-with-ts/src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// 타입 단언
+// ts 컴파일러가 null, undefined 가 가능하다라고 해석하고 있고,
+// 우린 #root가 존재한다고 단언할 수 있는 상황.
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
